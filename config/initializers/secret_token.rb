1d00090aee2579f8f5298100f6da2e3ee2170450
Neil Edwards <neil@plasticwater.com>
2012-06-11 14:46:37 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeetingRooms::Application.config.secret_token = 'ecf326d28d47eda56c989abb2cb884b7065767a19b6283f7ecb73eca0e0b28a46e0ee68b3a148565bb111c09876b955596caa512f33b7b91637e00b50fbe5ed7'
