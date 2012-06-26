95e801e90a9f6ed2c9693bd0ebe1c296c988233f
Neil Edwards <neil@plasticwater.com>
2012-06-26 13:14:58 +0100

    attempting to create an office on a new user being created

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create a business if one doesn't exist" do
+    user = User.create
+    user.office.should_not be_nil
+  end
 end
